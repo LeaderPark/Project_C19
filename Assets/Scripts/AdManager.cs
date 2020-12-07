bd9a2af5856736afab5fcf68e44d4986e587b833
cunoma <ggm.s200029@ggh.goe.go.kr>
2020-12-07 16:23:29 +0900

    섹스세긋

--- Assets/Scripts/AdManager.cs
+++ Assets/Scripts/AdManager.cs
@@ -1,83 +1,83 @@
-﻿using System.Collections;
-using UnityEngine;
-using UnityEngine.Advertisements;
-using System;
+﻿////using system.collections;
+////using unityengine;
+////using unityengine.advertisements;
+////using system;
 
-public class AdManager : MonoBehaviour, IUnityAdsListener
-{
-    string gameId = "3907975";
-    string myPlacementId = "GGMC19";
-    
-
-    private void Awake()
-    {
-        ShowAd();
-    }
+////public class admanager : monobehaviour, iunityadslistener
+////{
+//    string gameId = "3907975";
+//    string myPlacementId = "GGMC19";
 
 
-    private void Start()
-    {
-        Advertisement.AddListener(this);
-        Advertisement.Initialize(gameId);
-    }
+//    private void Awake()
+//    {
 
-    private void OnDisable()
-    {
-        Advertisement.RemoveListener(this);
-    }
+//    }
 
-    public void ShowAd()
-    {
-        StartCoroutine(ShowAdWhenReady());
-    }
 
-    IEnumerator ShowAdWhenReady()
-    {
-        while (!Advertisement.IsReady(myPlacementId))
-        {
-            yield return null;
-        }
-        Advertisement.Show(myPlacementId);
-    }
+//    private void Start()
+//    {
+//        Advertisement.AddListener(this);
+//        Advertisement.Initialize(gameId);
+//    }
 
-    #region UnityAdLister interfaces
-    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
-    {
-        if (showResult == ShowResult.Finished)
-        {
-            // Do something here.
-        }
-        else if (showResult == ShowResult.Skipped)
-        {
-            // Do something here;
-        }
-        else if (showResult == ShowResult.Failed)
-        {
-            Debug.LogWarning("The ad did not fhinish due to an error");
-        }
-    }
+//    private void OnDisable()
+//    {
+//        Advertisement.RemoveListener(this);
+//    }
 
-    public void OnUnityAdsReady(string placementId)
-    {
-        if (placementId == myPlacementId)
-        {
-            //  Advertisement.Show(myPlacementId);
-        }
+//    public void ShowAd()
+//    {
+//        StartCoroutine(ShowAdWhenReady());
+//    }
 
-    }
+//    IEnumerator ShowAdWhenReady()
+//    {
+//        while (!Advertisement.IsReady(myPlacementId))
+//        {
+//            yield return null;
+//        }
+//        Advertisement.Show(myPlacementId);
+//    }
 
-    public void OnUnityAdsDidError(string message)
-    {
-        Debug.LogError("OnUnityAdsDidError :" + message);
-    }
+//    #region UnityAdLister interfaces
+//    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+//    {
+//        if (showResult == ShowResult.Finished)
+//        {
+//            // Do something here.
+//        }
+//        else if (showResult == ShowResult.Skipped)
+//        {
+//            // Do something here;
+//        }
+//        else if (showResult == ShowResult.Failed)
+//        {
+//            Debug.LogWarning("The ad did not fhinish due to an error");
+//        }
+//    }
 
-    public void OnUnityAdsDidStart(string placementId)
-    {
-        if (placementId == myPlacementId)
-        {
-            //  Advertisement.Show(myPlacementId);
-        }
-    }
+//    public void OnUnityAdsReady(string placementId)
+//    {
+//        if (placementId == myPlacementId)
+//        {
+//            //  Advertisement.Show(myPlacementId);
+//        }
 
-    #endregion
-}
+//    }
+
+//    public void OnUnityAdsDidError(string message)
+//    {
+//        Debug.LogError("OnUnityAdsDidError :" + message);
+//    }
+
+//    public void OnUnityAdsDidStart(string placementId)
+//    {
+//        if (placementId == myPlacementId)
+//        {
+//            //  Advertisement.Show(myPlacementId);
+//        }
+//    }
+
+//    #endregion
+//}
